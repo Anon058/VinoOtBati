8966dd7f266b17a252c839e03fd93dc9a6be07ba
Malcev_AA <Malcev_AA@ISTIS-COL-10310>
2025-05-20 08:26:08 +0300

    Добавьте файлы проекта.

---
+++ VinoOtBati/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VinoOtBati
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DBEntities db = new DBEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var enter = db.Users.FirstOrDefault(x => x.UserName == UsernameTextBox.Text && x.Password == PasswordBox.Password);
+            
+            if(enter == null)
+            {
+                MessageBox.Show("Такого пользователя нет!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+            MessageBox.Show($"Добро пожаловать, {UsernameTextBox.Text}!", "Успешный вход", MessageBoxButton.OK, MessageBoxImage.Information);
+            var productsWindow = new ProductWindow(UsernameTextBox.Text);
+            productsWindow.Show();
+            this.Close();
+            }
+
+        }
+
+        private void GuestLink_Click(object sender, RoutedEventArgs e)
+        {
+            var productsWindow = new ProductWindow(null);
+            productsWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ VinoOtBati/ProductWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VinoOtBati
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
+    {
+        DBEntities db = new DBEntities();
+        public ProductWindow(string username)
+        {
+            InitializeComponent();
+            personListView.ItemsSource = db.Products.ToList();
+            if (string.IsNullOrEmpty(username))
+            {
+                WelcomeText.Text = "Вы вошли как гость";
+                Title = "Просмотр товаров (Гость)";
+            }
+            else
+            {
+                WelcomeText.Text = $"Добро пожаловать, {username}!";
+                Title = $"Просмотр товаров ({username})";
+            }
+        }
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(e.AddedItems.Count > 0)
+            {
+                Products products = (Products)e.AddedItems[0];
+
+                string ProductName = products.ProductName;
+
+            }
+        }
+    }
+}
